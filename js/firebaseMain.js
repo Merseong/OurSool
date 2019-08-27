ccc2ea231e7ac6d1a2573155e3e9b7509f2d243b
Merseong <esc990720@korea.ac.kr>
2019-08-27 23:06:35 +0900

    implement login/logout
    using firebase-auth

--- js/firebaseMain.js
+++ js/firebaseMain.js
@@ -1,1 +1,71 @@
-function firebaseMain() {}
+function FirebaseMain() {
+    this.init();
+    this.initEvent();
+}
+
+FirebaseMain.prototype.init = function() {
+    this.auth = firebase.auth();
+    this.googleBtn = document.getElementById("googleBtn");
+};
+
+FirebaseMain.prototype.initEvent = function() {
+    this.auth.onAuthStateChanged(this.onAuthChange.bind(this));
+
+    this.googleBtn.addEventListener("click", this.onGoogleBtnClick.bind(this));
+};
+
+FirebaseMain.prototype.onAuthChange = function(user) {
+    if (user) {
+        console.log("Login");
+        this.setLogin();
+    } else {
+        console.log("LogOut");
+        this.setLogOut();
+    }
+};
+
+FirebaseMain.prototype.setLogin = function() {
+    this.db = firebase.firestore();
+
+    $("#modalLogin").modal("hide");
+    document.getElementById("dvLoginSelect").style.display = "none";
+    document.getElementById("dvAlcAdder").style.display = "block";
+};
+
+FirebaseMain.prototype.setLogOut = function() {
+    document.getElementById("dvLoginSelect").style.display = "block";
+    document.getElementById("dvAlcAdder").style.display = "none";
+};
+
+FirebaseMain.prototype.onGoogleBtnClick = function() {
+    var googleProvider = new firebase.auth.GoogleAuthProvider();
+    this.auth
+        .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(
+            function() {
+                this.auth
+                    .signInWithPopup(googleProvider)
+                    .then(function(result) {
+                        console.log("google", result);
+                    })
+                    .catch(function(error) {
+                        console.error("error", error);
+                    });
+            }.bind(this)
+        )
+        .catch(function(error) {
+            console.error("인증 상태 설정 중 에러 발생", error);
+        });
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    window.fbMain = new FirebaseMain();
+
+    document.onkeydown = function(e) {
+        if (e.keyCode === 27 && confirm("LogOut?")) {
+            fbMain.auth.signOut();
+        }
+    };
+
+    console.log("done load");
+});
